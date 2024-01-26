7dc68213004b87bf52b8f4000b7642ccf887bf70
Emanuele Boattini <emanuele.boattini@gmail.com>
2024-01-26 16:07:07 +0100

    set up unit

--- java/src/main/java/tech/qmates/openchat/domain/UserNotFoundException.java
+++ java/src/main/java/tech/qmates/openchat/domain/UserNotFoundException.java
@@ -6,4 +6,7 @@ public class UserNotFoundException extends Exception {
     public UserNotFoundException(UUID uuid) {
         super("User with uuid [" + uuid + "] not found!");
     }
+    public UserNotFoundException(String username) {
+        super("User with username [" + username + "] not found!");
+    }
 }
---
+++ java/src/test/java/unit/tech/qmates/openchat/domain/usecase/LoginUseCaseTest.java
@@ -0,0 +1,10 @@
+package unit.tech.qmates.openchat.domain.usecase;
+
+import org.junit.jupiter.api.Test;
+
+public class LoginUseCaseTest {
+    @Test
+    void whenUserNameDoesNotExistThenBoh() {
+
+    }
+}
